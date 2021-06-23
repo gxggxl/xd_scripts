35014552c16c134452b0b749e4b9547e63301418
Annyoo2021 <61466497+Annyoo2021@users.noreply.github.com>
2021-06-23 15:57:35 +0800

    关闭助力作者

--- jd_jxlhb.js
+++ jd_jxlhb.js
@@ -48,7 +48,7 @@ const BASE_URL = 'https://wq.jd.com/cubeactive/steprewardv3'
       '活动入口：京喜app-》我的-》京喜领88元红包\n' +
       '助力逻辑：先自己京东账号相互助力，如有剩余助力机会，则助力作者\n' +
       '温馨提示：如提示助力火爆，可尝试寻找京东客服')
-//let res = await getAuthorShareCode() || [];
+  let res = await getAuthorShareCode() || [];
   let res2 = await getAuthorShareCode('') || [];
   if (res && res.activeId) $.activeId = res.activeId;
   $.authorMyShareIds = [...((res && res.codes) || []), ...res2];
