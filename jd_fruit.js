ad080a26d0a46406fa60380ec86049ed971e2521
Annyoo2021 <61466497+Annyoo2021@users.noreply.github.com>
2021-06-26 17:46:39 +0800

    去除作者助力

--- jd_fruit.js
+++ jd_fruit.js
@@ -1251,11 +1251,11 @@ function timeFormat(time) {
 }
 function readShareCode() {
   return new Promise(async resolve => {
-    $.get({url: `http://share.turinglabs.net/api/v3/farm/query/${randomCount}/`, timeout: 10000,}, (err, resp, data) => {
+    $.get({url: `}/`, timeout: 10000,}, (err, resp, data) => {
       try {
         if (err) {
-          console.log(`${JSON.stringify(err)}`)
-          console.log(`${$.name} API请求失败，请检查网路重试`)
+        // console.log(`${JSON.stringify(err)}`)
+        // console.log(`${$.name} API请求失败，请检查网路重试`)
         } else {
           if (data) {
             console.log(`随机取个${randomCount}码放到您固定的互助码后面(不影响已有固定互助)`)
