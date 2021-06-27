7511ab852d16fae1c81309f8fae37d0bd1874413
Annyoo2021 <61466497+Annyoo2021@users.noreply.github.com>
2021-06-27 14:57:03 +0800

    Update 过期活动的定时.sh

--- docker/过期活动的定时.sh
+++ docker/过期活动的定时.sh
@@ -9,4 +9,10 @@
 #跳跳乐瓜分京豆(已结束，注释)
 #15 0-23/2 * * * node /scripts/jd_jump.js >> /scripts/logs/jd_jump.log 2>&1
 
+# 宠汪汪喂食，修复版已集成，建议注释，避免重复。
+#*/20 0-23 * * * node /scripts/jd_joy_feedPets.js >> /scripts/logs/jd_joy_feedPets.log 2>&1
+
+# 宠汪汪邀请助力，修复版已集成，建议注释，避免重复。
+#10 6-20/2 * * * node /scripts/jd_joy_run.js >> /scripts/logs/jd_joy_run.log 2>&1
+
 
