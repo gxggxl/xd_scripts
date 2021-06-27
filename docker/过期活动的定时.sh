7e1353a99b35ca56807f8eb937b9fbee9aba6d5d
Annyoo2021 <61466497+Annyoo2021@users.noreply.github.com>
2021-06-27 14:53:01 +0800

    Update 过期活动的定时.sh

--- docker/过期活动的定时.sh
+++ docker/过期活动的定时.sh
@@ -1,8 +1,9 @@
 ####################过期活动的定时####################
 
-#京喜领88元红包(已结束，注释)
+#京喜领88元红包(已结束.注释)
 #30 1,6,12,21 * * * node /scripts/jd_jxlhb.js >> /scripts/logs/jd_jxlhb.log 2>&1
 
-
+#省钱大赢家之翻翻乐(已结束.注释)
+#10,40 * * * * node /scripts/jd_big_winner.js >> /scripts/logs/jd_big_winner.log 2>&1
 
 
