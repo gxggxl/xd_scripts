23f0962a6dd9b7acd1b924c09325859fad153919
Annyoo2021 <61466497+Annyoo2021@users.noreply.github.com>
2021-06-27 15:18:13 +0800

    Update 过期活动的定时.sh

--- docker/过期活动的定时.sh
+++ docker/过期活动的定时.sh
@@ -1,12 +1,12 @@
 ####################过期活动的定时####################
 
-# 京喜领88元红包(已结束.注释)
+# 京喜领88元红包(已结束)
 #30 1,6,12,21 * * * node /scripts/jd_jxlhb.js >> /scripts/logs/jd_jxlhb.log 2>&1
 
-# 省钱大赢家之翻翻乐(已结束.注释)
+# 省钱大赢家之翻翻乐(已结束)
 #10,40 * * * * node /scripts/jd_big_winner.js >> /scripts/logs/jd_big_winner.log 2>&1
 
-# 跳跳乐瓜分京豆(已结束，注释)
+# 跳跳乐瓜分京豆(已结束)
 #15 0-23/2 * * * node /scripts/jd_jump.js >> /scripts/logs/jd_jump.log 2>&1
 
 # 宠汪汪喂食，修复版已集成，建议注释，避免重复。
@@ -21,10 +21,21 @@
 # 宠汪汪偷好友积分与狗粮(旧版.不可用)
 #10 0-21/3 * * * node /scripts/jd_joy_steal.js >> /scripts/logs/jd_joy_steal.log 2>&1
 
-# 京东手机狂欢城(已结束，注释)
+# 京东手机狂欢城(已结束)
 #0 0,12,18,21 * * * node /scripts/jd_carnivalcity.js >> /scripts/logs/jd_carnivalcity.log 2>&1
 
-# 新潮品牌狂欢(已结束，注释)
+# 新潮品牌狂欢(已结束)
 #20 1,21 * * * node /scripts/jd_mcxhd.js >> /scripts/logs/jd_mcxhd.log 2>&1
+
+# 家电星推官(已结束)
+#0 0 * * * node /scripts/jd_xtg.js >> /scripts/logs/jd_xtg.log 2>&1
+
+# 家电星推官好友互助(已结束)
+#0 0 * * * node /scripts/jd_xtg_help.js >> /scripts/logs/jd_xtg_help.log 2>&1
+
+
+
+
+
 
 
