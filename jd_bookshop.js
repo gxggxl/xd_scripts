f3652c78adf4e9da8686febcc2d458c15e6dfd00
Annyoo2021 <61466497+Annyoo2021@users.noreply.github.com>
2021-07-01 20:34:06 +0800

    口袋书店又回来了

--- jd_bookshop.js
+++ jd_bookshop.js
@@ -705,7 +705,6 @@ function shareCodesFormat() {
     if (readShareCodeRes && readShareCodeRes.code === 200) {
       $.newShareCodes = [...new Set([...$.newShareCodes, ...(readShareCodeRes.data || [])])];
     }
-    $.newShareCodes.map((item, index) => $.newShareCodes[index] = { "inviteCode": item, "shareDate": $.shareDate })
     console.log(`第${$.index}个京东账号将要助力的好友${JSON.stringify($.newShareCodes)}`)
     resolve();
   })
