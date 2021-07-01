2253063a69cbb4db24e7cf5489b4decc59372d23
Annyoo2021 <61466497+Annyoo2021@users.noreply.github.com>
2021-07-01 20:15:09 +0800

    口袋书店又回来了

--- jd_bookshop.js
+++ jd_bookshop.js
@@ -32,10 +32,9 @@ let ADD_CART = false
 ADD_CART = $.isNode() ? (process.env.PURCHASE_SHOPS ? process.env.PURCHASE_SHOPS : ADD_CART) : ($.getdata("ADD_CART") ? $.getdata("ADD_CART") : ADD_CART);
 // 加入购物车开关，与东东小窝共享
 
-let inviteCodes = [
-  '28a699ac78d74aa3b31f7103597f8927@dbffb1e337174317a6482c237a871bfd@2f14ee9c92954cf79829320dd482bf49@fdf827db272543d88dbb51a505c2e869@ce2536153a8742fb9e8754a9a7d361da@38ba4e7ba8074b78851e928af2b4f6b2',
-  '28a699ac78d74aa3b31f7103597f8927@dbffb1e337174317a6482c237a871bfd@2f14ee9c92954cf79829320dd482bf49@fdf827db272543d88dbb51a505c2e869'
-]
+// let inviteCodes = [
+// '28a699ac78d74aa3b31f7103597f8927@dbffb1e337174317a6482c237a871bfd@2f14ee9c92954cf79829320dd482bf49@fdf827db272543d88dbb51a505c2e869'
+// ]
 
 if ($.isNode()) {
   Object.keys(jdCookieNode).forEach((item) => {
@@ -696,11 +695,12 @@ function shareCodesFormat() {
     $.newShareCodes = [];
     if ($.shareCodesArr[$.index - 1]) {
       $.newShareCodes = $.shareCodesArr[$.index - 1].split('@');
-    } else {
-      console.log(`由于您第${$.index}个京东账号未提供shareCode,将采纳本脚本自带的助力码\n`)
-      const tempIndex = $.index > inviteCodes.length ? (inviteCodes.length - 1) : ($.index - 1);
-      $.newShareCodes = inviteCodes[tempIndex].split('@');
     }
+    // else {
+    //  console.log(`由于您第${$.index}个京东账号未提供shareCode,将采纳本脚本自带的助力码\n`)
+    //  const tempIndex = $.index > inviteCodes.length ? (inviteCodes.length - 1) : ($.index - 1);
+    //  $.newShareCodes = inviteCodes[tempIndex].split('@');
+    // }
     const readShareCodeRes = null //await readShareCode();
     if (readShareCodeRes && readShareCodeRes.code === 200) {
       $.newShareCodes = [...new Set([...$.newShareCodes, ...(readShareCodeRes.data || [])])];
