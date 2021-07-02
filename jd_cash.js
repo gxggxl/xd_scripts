7b1831d0f485d67c2048c92b4b8e5fdfa14969e8
Annyoo2021 <61466497+Annyoo2021@users.noreply.github.com>
2021-07-02 13:39:05 +0800

    去除作者助力

--- jd_cash.js
+++ jd_cash.js
@@ -31,8 +31,7 @@ let helpAuthor = false;
 const randomCount = $.isNode() ? 0 : 5;
 let cash_exchange = false;//是否消耗2元红包兑换200京豆，默认否
 const inviteCodes = [
-  `IRs2ajk74iqlabtjdw@9bavtFU4sE2_827Uwg@95CiuWMEsnSsKNt3yXMR1w`,
-  `IRs2ajk74iqlabtjdw@9bavtFU4sE2_827Uwg@95CiuWMEsnSsKNt3yXMR1w`
+  `IRs2ajk74iqlabtjdw@9bavtFU4sE2_827Uwg@9IeWuGU8s1GynmzVyXI@95CiuWMEsnSsKNt3yXMR1w`
 ]
 if ($.isNode()) {
   Object.keys(jdCookieNode).forEach((item) => {
