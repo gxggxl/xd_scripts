39ab94712ed61ce4ba21bece63cf89710eecc05c
Annyoo2021 <61466497+Annyoo2021@users.noreply.github.com>
2021-07-03 21:50:59 +0800

    Update 过期活动的定时.sh

--- docker/过期活动的定时.sh
+++ docker/过期活动的定时.sh
@@ -12,9 +12,6 @@
 # 宠汪汪强制为别人助力(旧版.不可用)
 #15 10 * * * node /scripts/jd_joy_help.js >> /scripts/logs/jd_joy_help.log 2>&1
 
-# 宠汪汪偷好友积分与狗粮(旧版.不可用)
-#10 0-21/3 * * * node /scripts/jd_joy_steal.js >> /scripts/logs/jd_joy_steal.log 2>&1
-
 # 京东手机狂欢城(已结束)
 #0 0,12,18,21 * * * node /scripts/jd_carnivalcity.js >> /scripts/logs/jd_carnivalcity.log 2>&1
 
