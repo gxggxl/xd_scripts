2a32db8ba579cb3a0aefff78f9e5fa34f0a260a5
Annyoo2021 <61466497+Annyoo2021@users.noreply.github.com>
2021-07-06 19:53:51 +0800

    删除残余助力码

--- jd_dreamFactory.js
+++ jd_dreamFactory.js
@@ -42,13 +42,7 @@ const randomCount = $.isNode() ? 0 : 5;
 let tuanActiveId = ``, hasSend = false;
 const jxOpenUrl = `openjd://virtual?params=%7B%20%22category%22:%20%22jump%22,%20%22des%22:%20%22m%22,%20%22url%22:%20%22https://wqsd.jd.com/pingou/dream_factory/index.html%22%20%7D`;
 let cookiesArr = [], cookie = '', message = '', allMessage = '';
-const inviteCodes = [
-  '0PbNMyCQ_Dpz_gkADOZlnA==',
-  "wcEF7ysifWiPT_ozkpTsCQ==",
-  "HzBraHpFnKCWr6rvyZZkXQ==",
-  '53gzd022_wSsWlN4OfOypA==',
-  'RD9XNkB7AyRkEonwjIAMZQ=='
-];
+const inviteCodes = [];
 const jdCookieNode = $.isNode() ? require('./jdCookie.js') : '';
 $.tuanIds = [];
 $.appId = 10001;
