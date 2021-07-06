c1ed512a6c00d598d18019039eed804fcfd58bbc
Annyoo2021 <61466497+Annyoo2021@users.noreply.github.com>
2021-07-06 20:01:00 +0800

    去除助力

--- jd_pet.js
+++ jd_pet.js
@@ -31,9 +31,9 @@ let cookiesArr = [], cookie = '', jdPetShareArr = [], isBox = false, notify, new
 //下面给出两个账号的填写示例（iOS只支持2个京东账号）
 let shareCodes = [ // IOS本地脚本用户这个列表填入你要助力的好友的shareCode
    //账号一的好友shareCode,不同好友的shareCode中间用@符号隔开
-  'MTE1NDQ5MzYwMDAwMDAwNDM4MTM4MDU=@MTEzMzI0OTE0NTAwMDAwMDA0MzgxMzg2Mw==@MTEyNjkzMjAwMDAwMDAwMDUwOTk5MTYz@MTEzMzI0OTE0NTAwMDAwMDA0Mzg2NTA5MQ==',
+  '',
   //账号二的好友shareCode,不同好友的shareCode中间用@符号隔开
-  'MTE1NDQ5MzYwMDAwMDAwNDM4MTM4MDU=@MTEzMzI0OTE0NTAwMDAwMDA0MzgxMzg2Mw==@MTEyNjkzMjAwMDAwMDAwMDUwOTk5MTYz@MTEzMzI0OTE0NTAwMDAwMDA0Mzg2NTA5MQ==',
+  '',
 ]
 let message = '', subTitle = '', option = {};
 let jdNotify = false;//是否关闭通知，false打开通知推送，true关闭通知推送
