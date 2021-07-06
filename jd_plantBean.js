9ed785fa8d79ff2b5b1f73e8e4d37722323ede44
Annyoo2021 <61466497+Annyoo2021@users.noreply.github.com>
2021-07-06 20:00:37 +0800

    去除助力

--- jd_plantBean.js
+++ jd_plantBean.js
@@ -35,9 +35,9 @@ const JD_API_HOST = 'https://api.m.jd.com/client.action';
 //下面给出两个账号的填写示例（iOS只支持2个京东账号）
 let shareCodes = [ // IOS本地脚本用户这个列表填入你要助力的好友的shareCode
                    //账号一的好友shareCode,不同好友的shareCode中间用@符号隔开
-  'gtff3aeremgzgiz76zarkcx5awez2zowztpyv3y@sbzsc26pxgymgwzk6mzmm3yvzoolettlf7pcdzi@rnuuqx36begu7eoqp7zpx5c5zgajivqtr4pk2cq@ifroxmwjcynbkhuauqjxa2ut4f5k4n6vghvouzy',
+  '',
   //账号二的好友shareCode,不同好友的shareCode中间用@符号隔开
-  'gtff3aeremgzgiz76zarkcx5awez2zowztpyv3y@sbzsc26pxgymgwzk6mzmm3yvzoolettlf7pcdzi@rnuuqx36begu7eoqp7zpx5c5zgajivqtr4pk2cq@ifroxmwjcynbkhuauqjxa2ut4f5k4n6vghvouzy',
+  '',
 ]
 let allMessage = ``;
 let currentRoundId = null;//本期活动id
