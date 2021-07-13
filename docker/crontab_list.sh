483b476d30f94d7f2853ea0b1cc92823a7fb7192
Annyoo2021 <61466497+Annyoo2021@users.noreply.github.com>
2021-07-13 16:08:52 +0800

    Update crontab_list.sh

--- docker/crontab_list.sh
+++ docker/crontab_list.sh
@@ -17,9 +17,6 @@
 # 京享值PK 活动时间：6.22-7.21
 11 0,6,11,16,21 5-21 7 * node /scripts/jd_jxzpk.js >> /scripts/logs/jd_jxzpk.log 2>&1
 
-# 欧洲狂欢杯 活动时间：###
-0 5,10,11 * * * node /scripts/jd_europeancup.js >> /scripts/logs/jd_europeancup.log 2>&1
-
 # 星系牧场 #wen
 1 */2 * * * node /scripts/jd_qqxing.js >> /scripts/logs/jd_qqxing.log 2>&1
 
