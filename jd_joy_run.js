87a5ddfba9a83d3d32cc38e4b70a965466a4c16a
Annyoo2021 <61466497+Annyoo2021@users.noreply.github.com>
2021-07-19 12:27:49 +0800

    Update jd_joy_run.js

--- jd_joy_run.js
+++ jd_joy_run.js
@@ -12,6 +12,9 @@ token获取途径：
 
 docker 设置环境变量 JOY_RUN_HELP_MYSELF 为true,则开启账号内部互助.默认关闭(即给脚本作者内置的助力).
 
+## 宠汪汪赛跑开启自己账号内部互助
+export JOY_RUN_HELP_MYSELF="true"
+
 [MITM]
 hostname = draw.jdfcloud.com
 
