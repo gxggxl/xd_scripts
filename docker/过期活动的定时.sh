8d38642d5fcdf6a158d7631f68336f55c06e9472
Annyoo2021 <61466497+Annyoo2021@users.noreply.github.com>
2021-07-22 12:44:01 +0800

    Update 过期活动的定时.sh

--- docker/过期活动的定时.sh
+++ docker/过期活动的定时.sh
@@ -1,5 +1,8 @@
 ####################过期活动的定时####################
 
+# 京享值PK 活动时间：6.22-7.21
+11 0,6,11,16,21 5-21 7 * node /scripts/jd_jxzpk.js >> /scripts/logs/jd_jxzpk.log 2>&1
+
 # 明星小店(星店长)
 0 1,21 * * * node /scripts/jd_star_shop.js >> /scripts/logs/jd_star_shop.log 2>&1
 
