57da81d262a13367baf5f8cbe5977ab5ee5a7560
gxggxl <gxggxl@qq.com>
2021-07-23 17:53:42 +0800

    燃动夏季

--- docker/OverdueEvent_crontab_list.sh
+++ docker/OverdueEvent_crontab_list.sh
@@ -42,11 +42,5 @@
 # 618动物联萌收金币(已结束)
 #0-59/30 * * * * node /scripts/jd_zooCollect.js >> /scripts/logs/jd_zooCollect.log 2>&1
 
-# 燃动夏季 活动时间：7.8-8.8
-20 0,6-23/2 * 7-8 * node /scripts/jd_summer_movement.js >> /scripts/logs/jd_summer_movement.log 2>&1
-
-# 燃动夏季百元守卫战_助力 活动时间：7.8-8.8
-12 7-14 * 7-8 * node /scripts/jd_summer_movement_help.js >> /scripts/logs/jd_summer_movement_help.log 2>&1
-
 # 特物Z|万物皆可国创 活动时间：###
 3 0,13 * * * node /scripts/jd_superBrand.js >> /scripts/logs/jd_superBrand.log 2>&1
