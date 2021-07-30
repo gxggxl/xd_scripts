d9cfb3447ab3e57640a70592b12cd0576527d23b
gxggxl <gxggxl@qq.com>
2021-07-30 11:43:58 +0800

    热气球挂机

--- docker/proc_file.sh
+++ docker/proc_file.sh
@@ -6,22 +6,43 @@ else
   CMD="node"
 fi
 
-echo "处理jd_crazy_joy_coin任务。。。"
+#echo "处理jd_crazy_joy_coin任务。。。"
+#if [ ! $CRZAY_JOY_COIN_ENABLE ]; then
+#   echo "默认启用jd_crazy_joy_coin杀掉jd_crazy_joy_coin任务，并重启"
+#   eval $(ps -ef | grep "jd_crazy_joy_coin" | grep -v "grep" | awk '{print "kill "$1}')
+#   echo '' >/scripts/logs/jd_crazy_joy_coin.log
+#   $CMD /scripts/jd_crazy_joy_coin.js | ts >>/scripts/logs/jd_crazy_joy_coin.log 2>&1 &
+#   echo "默认jd_crazy_joy_coin重启完成"
+#else
+#   if [ $CRZAY_JOY_COIN_ENABLE = "Y" ]; then
+#      echo "配置启用jd_crazy_joy_coin，杀掉jd_crazy_joy_coin任务，并重启"
+#      eval $(ps -ef | grep "jd_crazy_joy_coin" | grep -v "grep" | awk '{print "kill "$1}')
+#      echo '' >/scripts/logs/jd_crazy_joy_coin.log
+#      $CMD /scripts/jd_crazy_joy_coin.js | ts >>/scripts/logs/jd_crazy_joy_coin.log 2>&1 &
+#      echo "配置jd_crazy_joy_coin重启完成"
+#   else
+#      eval $(ps -ef | grep "jd_crazy_joy_coin" | grep -v "grep" | awk '{print "kill "$1}')
+#      echo "已配置不启用jd_crazy_joy_coin任务，仅杀掉"
+#   fi
+#fi
+
+
+echo "处理jd_cfd_loop任务。。。"
 if [ ! $CRZAY_JOY_COIN_ENABLE ]; then
-   echo "默认启用jd_crazy_joy_coin杀掉jd_crazy_joy_coin任务，并重启"
-   eval $(ps -ef | grep "jd_crazy_joy_coin" | grep -v "grep" | awk '{print "kill "$1}')
-   echo '' >/scripts/logs/jd_crazy_joy_coin.log
-   $CMD /scripts/jd_crazy_joy_coin.js | ts >>/scripts/logs/jd_crazy_joy_coin.log 2>&1 &
-   echo "默认jd_crazy_joy_coin重启完成"
+   echo "默认启用jd_cfd_loop杀掉jd_cfd_loop任务，并重启"
+   eval $(ps -ef | grep "jd_cfd_loop" | grep -v "grep" | awk '{print "kill "$1}')
+   echo '' >/scripts/logs/jd_cfd_loop.log
+   $CMD /scripts/jd_cfd_loop.js | ts >>/scripts/logs/jd_cfd_loop.log 2>&1 &
+   echo "默认jd_cfd_loop重启完成"
 else
    if [ $CRZAY_JOY_COIN_ENABLE = "Y" ]; then
-      echo "配置启用jd_crazy_joy_coin，杀掉jd_crazy_joy_coin任务，并重启"
-      eval $(ps -ef | grep "jd_crazy_joy_coin" | grep -v "grep" | awk '{print "kill "$1}')
-      echo '' >/scripts/logs/jd_crazy_joy_coin.log
-      $CMD /scripts/jd_crazy_joy_coin.js | ts >>/scripts/logs/jd_crazy_joy_coin.log 2>&1 &
-      echo "配置jd_crazy_joy_coin重启完成"
+      echo "配置启用jd_cfd_loop，杀掉jd_cfd_loop任务，并重启"
+      eval $(ps -ef | grep "jd_cfd_loop" | grep -v "grep" | awk '{print "kill "$1}')
+      echo '' >/scripts/logs/jd_cfd_loop.log
+      $CMD /scripts/jd_cfd_loop.js | ts >>/scripts/logs/jd_cfd_loop.log 2>&1 &
+      echo "配置jd_cfd_loop重启完成"
    else
-      eval $(ps -ef | grep "jd_crazy_joy_coin" | grep -v "grep" | awk '{print "kill "$1}')
-      echo "已配置不启用jd_crazy_joy_coin任务，仅杀掉"
+      eval $(ps -ef | grep "jd_cfd_loop" | grep -v "grep" | awk '{print "kill "$1}')
+      echo "已配置不启用jd_cfd_loop任务，仅杀掉"
    fi
 fi
