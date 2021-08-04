8e34a334b45b63ad343f7ef164400f494172b88d
gxggxl <gxggxl@qq.com>
2021-08-05 00:25:15 +0800

    Update jd_dj_fruit.js

--- jd_dj_fruit.js
+++ jd_dj_fruit.js
@@ -20,7 +20,7 @@ let ckPath = './jdCookie.js';//ck路径,环境变量:JDDJ_CKPATH
 const $ = new API("jd_dj_fruit");
 try {
     //https://ghproxy.com/https://raw.githubusercontent.com/passerby-b/Script/master/jddj_fruit_code.js
-    $.http.get({ url: 'https://raw.githubusercontent.com/Annyoo2021/scripts/main/utils/jd_dj_fruit_code.js' }).then(response => {
+    $.http.get({ url: 'https://ghproxy.com/https://raw.githubusercontent.com/Annyoo2021/scripts/main/utils/jd_dj_fruit_code.js' }).then(response => {
         if (!!response.body) eval(response.body);
     });
 } catch (error) {
