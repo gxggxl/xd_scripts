981b6826908bf6773fe515b6f67b64881139d609
gxggxl <gxggxl@qq.com>
2021-08-09 19:15:15 +0800

    add 金机奖投票

--- docker/crontab_list.sh
+++ docker/crontab_list.sh
@@ -7,6 +7,9 @@
 
 ##############短期活动##############
 
+# 金机奖投票
+35 1,7 8-20 8 * node /scripts/jd_golden_machine.js >> /scripts/logs/jd_golden_machine.log 2>&1
+
 # 小鸽有礼 - 每日抽奖(活动时间：2021-05-01至2021-05-31)
 13 1,22,23 * * * node /scripts/jd_daily_lottery.js >> /scripts/logs/jd_daily_lottery.log 2>&1
 
