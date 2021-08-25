9b17b935b38ceb9ea023dd22a8020965f64de5a2
gxggxl <gxggxl@qq.com>
2021-08-25 23:55:13 +0800

    Update update_list.sh

--- update_list.sh
+++ update_list.sh
@@ -10,9 +10,7 @@ WorkDir=$(
   pwd
 )
 
-# shellcheck disable=SC2207
-# shellcheck disable=SC2010
-JsList=($(cd "$WorkDir" || exit; ls -- *.js | grep -E "j[drx]_" | perl -ne "{print unless /\.bak/}"))
+JsList=($(cd "$WorkDir" && ls -- *.js | grep -E "j[drx]_" | perl -ne "{print unless /\.bak/}"))
 FileReadme=$WorkDir/list_README.md
 UrlRaw=https://github.com/gxggxl/xd_scripts/raw/master/
 SheetHead="# 脚本列表\n\n| 序号 | 文件 | 名称 | 活动入口 |\n| :---: | --- | --- | --- |"
@@ -26,13 +24,22 @@ SheetHead="# 脚本列表\n\n| 序号 | 文件 | 名称 | 活动入口 |\n| :---
 ## 生成新的表格并写入Readme
 cd "$WorkDir" || exit
 Sheet=$SheetHead
-for ((i = 1; i <= ${#JsList[*]}; i++)); do
-  Name=$(grep -E "new Env|Env" "${JsList[i]}" | awk -F "['\"]" '{print $2}' | head -1)
-  Entry=$(grep -E "^ *活动入口" "${JsList[i]}" | awk -F "：|: " '{print $2}' | head -1)
-  [[ -z $Entry ]] || [[ $Entry == 暂无 ]] && Entry=$(grep -E "^ *活动地址" "${JsList[i]}" | awk -F "：|: " '{print $2}' | head -1)
+#for ((i = 0; i <= ${#JsList[*]}; i++)); do
+#  Name=$(grep -E "new Env|Env" "${JsList[i]}" | awk -F "['\"]" '{print $2}' | head -1)
+#  Entry=$(grep -E "^ *活动入口" "${JsList[i]}" | awk -F "：|: " '{print $2}' | head -1)
+#  [[ -z $Entry ]] || [[ $Entry == 暂无 ]] && Entry=$(grep -E "^ *活动地址" "${JsList[i]}" | awk -F "：|: " '{print $2}' | head -1)
+#  [[ $Entry == http* ]] && Entry="[活动地址]($Entry)"
+#  Raw="$UrlRaw${JsList[i]}"
+#  Sheet="$Sheet\n| $((i)) | [${JsList[i]}]($Raw) | $Name | $Entry |"
+#done
+
+for jsFileName in "${JsList[@]}" ; do
+  Name=$(grep -E "new Env|Env" "$jsFileName" | awk -F "['\"]" '{print $2}' | head -1)
+  Entry=$(grep -E "^ *活动入口" "$jsFileName" | awk -F "：|: " '{print $2}' | head -1)
+  [[ -z $Entry ]] || [[ $Entry == 暂无 ]] && Entry=$(grep -E "^ *活动地址" "$jsFileName" | awk -F "：|: " '{print $2}' | head -1)
   [[ $Entry == http* ]] && Entry="[活动地址]($Entry)"
-  Raw="$UrlRaw${JsList[i]}"
-  Sheet="$Sheet\n| $((i)) | [${JsList[i]}]($Raw) | $Name | $Entry |"
+  Raw="$UrlRaw$jsFileName"
+  Sheet="$Sheet\n| $(((a++)+1)) | [$jsFileName]($Raw) | $Name | $Entry |"
 done
 #echo -e "$Sheet\n$Tail" >>"$FileReadme"
 echo -e "$Sheet" >"$FileReadme"
