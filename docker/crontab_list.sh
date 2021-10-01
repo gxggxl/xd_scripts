f81a9709526605c652051e168b055ebbb9f208ee
gxggxl <gxggxl@qq.com>
2021-10-01 10:31:58 +0800

    Update crontab_list.sh

--- docker/crontab_list.sh
+++ docker/crontab_list.sh
@@ -26,9 +26,6 @@
 # 企有此礼(9.30 结束)
 28 0 10-30 9 * node /scripts/jd_qycl.js >> /scripts/logs/jd_qycl.log 2>&1
 
-# 跳跳乐瓜分京豆(9.13-9.19)
-15 */2 20-27 9 * node /scripts/jd_jump.js >> /scripts/logs/jd_jump.log 2>&1
-
 # 京小鸽吾悦寄 (9.30 结束)
 9 1,13 1-30 9 * node /scripts/jd_jxg.js >> /scripts/logs/jd_jxg.log 2>&1
 
