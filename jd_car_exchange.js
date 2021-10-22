affb42fd73ea59c9b62ea0535a0c866380545aee
gxggxl <gxggxl@qq.com>
2021-10-22 10:08:07 +0800

    Update jd_car_exchange.js

--- jd_car_exchange.js
+++ jd_car_exchange.js
@@ -45,7 +45,7 @@ const JD_API_HOST = 'https://car-member.jd.com/api/';
         $.msg($.name, '【提示】请先获取京东账号一cookie\n直接使用NobyDa的京东签到获取', 'https://bean.m.jd.com/bean/signIndex.action', {"open-url": "https://bean.m.jd.com/bean/signIndex.action"});
         return;
     }
-    for (let j = 0; j < 8; ++j) {
+    for (let j = 0; j < 16; ++j) {
         for (let i = 0; i < cookiesArr.length; i++) {
             if (cookiesArr[i]) {
                 cookie = cookiesArr[i];
