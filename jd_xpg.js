1be2bc8c81a6db9da9a55f3345a320612775392a
gxggxl <gxggxl@qq.com>
2021-11-02 16:33:07 +0800

    Update jd_xpg.js

--- jd_xpg.js
+++ jd_xpg.js
@@ -74,7 +74,7 @@ let invitelist = [];
             }
             console.log(`\n${$.UserName}去助力${$.oneInvite.user},助力码:${$.oneInvite.inviter_id}`);
             await takePostRequest('invite_friend');
-            await $.wait(1000);
+            await $.wait(2000);
         }
     }
 })()
