96081d67abe032fca1133b4b8927c65aa8d96cad
gxggxl <gxggxl@qq.com>
2021-11-04 21:19:58 +0800

    add

--- docker/crontab_list.sh
+++ docker/crontab_list.sh
@@ -8,6 +8,8 @@
 3 0,12 * * * node /scripts/jd_ckcheck.js >> /scripts/logs/jd_ckcheck.log 2>&1
 
 ##############短期活动##############
+# 双11红包
+0 10,12,20 * * * node /scripts/jd_jxred.js >> /scripts/logs/jd_jxred.log 2>&1
 # 电器盲盒抽京豆 (11.20)
 0 1,8 1-20 11 * node /scripts/jd_dqmh.js >> /scripts/logs/jd_dqmh.log 2>&1
 # 牛牛福利
