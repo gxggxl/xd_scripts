6106b74d8c7917d6779c89d23558574d5123a959
gxggxl <gxggxl@qq.com>
2021-11-05 00:14:33 +0800

    Update update_list.sh

--- update_list.sh
+++ update_list.sh
@@ -19,7 +19,7 @@ JsList=($(cd "$WorkDir" && ls -- *.js | grep -E "j[drx]_" | grep -v jd_cash_ddo.
 FileReadme=$WorkDir/list_README.md
 UrlRaw=https://github.com/gxggxl/xd_scripts/raw/master/
 #SheetHead="# 脚本列表\n\n| 序号 | 文件 | 名称 | 活动入口 |\n| :---: | --- | --- | --- |"
-SheetHead="# 脚本列表\n\n共有 **"${#JsList[@]}"** 个文件\n\n| 文件 | 名称 | 活动入口 |\n| --- | --- | --- |"
+SheetHead="# 脚本列表\n\n共有 **"${#JsList[@]}"** 个文件\n\n| 文件 | 名称 | 活动入口 ｜ 活动时间 |\n| --- | --- | --- | --- |"
 
 ## 生成新的表格并写入Readme
 cd "$WorkDir" || exit
@@ -30,9 +30,10 @@ for jsFileName in "${JsList[@]}"; do
   Entry=$(grep -E "^( *|\s+\*\s+|\*\s+|\*)活动入口" "$jsFileName" | awk -F "：|: " '{print $2}' | head -1)
   [[ -z $Entry ]] || [[ $Entry == 暂无 ]] && Entry=$(grep -E "^( *|\s+\*\s+|\*\s+|\*)活动地址" "$jsFileName" | awk -F "：|: " '{print $2}' | head -1)
   [[ $Entry == http* ]] && Entry="[活动地址]($Entry)"
+  Time=$(grep -E "^( *|\s+\*\s+|\*\s+|\*)活动时间" "$jsFileName" | awk -F "：|: " '{print $2}' | head -1)
   Raw="$UrlRaw$jsFileName"
   #  Sheet="$Sheet\n| $(((a++)+1)) | [$jsFileName]($Raw) | $Name | $Entry |"
-  Sheet="$Sheet\n| [$jsFileName]($Raw) | $Name | $Entry |"
+  Sheet="$Sheet\n| [$jsFileName]($Raw) | $Name | $Entry | $Time |"
 done
 
 echo -e "$Sheet\n\n此内容仅供参考" > "$FileReadme"
