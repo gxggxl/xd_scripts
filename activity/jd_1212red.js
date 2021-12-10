e4d4d2bfa961aaa4f74ab68a5215d82096310e98
gxggxl <gxggxl@qq.com>
2021-12-10 19:33:24 +0800

    Update jd_1212red.js

--- activity/jd_1212red.js
+++ activity/jd_1212red.js
@@ -7,9 +7,9 @@
 cron "1 0,20 * * * " script-path=jd_1212red.js,tag=双十二红包领取
 */
 const $ = new Env('双十二红包领取');
-const notify = $.isNode() ? require('./sendNotify') : '';
+const notify = $.isNode() ? require('../sendNotify') : '';
 //Node.js用户请在jdCookie.js处填写京东ck;
-const jdCookieNode = $.isNode() ? require('./jdCookie.js') : '';
+const jdCookieNode = $.isNode() ? require('../jdCookie.js') : '';
 //IOS等用户直接用NobyDa的jd cookie
 let cookiesArr = [], cookie = '', message;
 
